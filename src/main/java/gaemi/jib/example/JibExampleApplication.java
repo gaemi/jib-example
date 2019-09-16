db01c8fc56aac0a891504ef423e5f94131560703
Gaemi <segaji@gmail.com>
2019-09-16 14:02:28 +0900

    first commit

---
+++ src/main/java/gaemi/jib/example/JibExampleApplication.java
@@ -0,0 +1,13 @@
+package gaemi.jib.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibExampleApplication.class, args);
+	}
+
+}
